e6979e87302362a90cfb9c605c708bea58e547f5
veljk <veljkolazic117@gmail.com>
2022-09-05 15:42:41 +0200

    neke izmene

--- apps/shop/start.sh
+++ apps/shop/start.sh
@@ -1,3 +1,19 @@
 #!/usr/bin/env sh
+# if [ "$APPLICATION" = "admin" ]; then
+#     echo STARTED ENTRY admin
+#     sleep 5
+#     while true; do
+#         python migrate.py
+#         if [ $? -eq 1 ]; then
+#             break
+#         fi
+#         echo sleeping 5
+#         sleep 5
+#     done
+#     echo FINISHED BOOTSTRAP
+# else
+#     echo sleeping 15
+#     sleep 15
+# fi
 python migrate.py
 python admin.py
